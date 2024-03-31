76df80c8bbb21493e0548edf04807a6f0a94d815
Ted Moke <iam@tedmoke.com>
2024-03-31 10:50:14 -0400

    Address edge cases on problems

--- problem_1-matrixSum.js
+++ problem_1-matrixSum.js
@@ -1,19 +1,58 @@
 // Medium Difficulty: (Do it in a single loop)
-// Given a 2D square matrix, find the sum of elements in Principal and Secondary diagonals. For example, consider the following 3 X 3 input matrix. 
-// Input: [[1,2,3], [4,5,6], [7,8,9]] 
+// Given a 2D square matrix, find the sum of elements in Principal and Secondary diagonals. For example, consider the following 3 X 3 input matrix.
+// Input: [[1,2,3], [4,5,6], [7,8,9]]
 
 function sumMatrixDiagonals(matrix) {
-    let sum = 0;
-    for (let i = 0; i < matrix.length; i++) {
-        sum += matrix[i][i];
-        sum += matrix[i][matrix.length - i - 1];
-    }
+  if (matrix.length === 0 || matrix[0].length < 1) return 0;
+  if (matrix.length !== matrix[0].length) return "Not a rectangle matrix";
+  if (matrix.length === 1) return matrix[0][0];
 
-    return sum;
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    sum += matrix[i][i];
+    sum += matrix[i][matrix.length - i - 1];
+  }
+
+  return sum;
 }
 
-const matrix = [[1,2,3], [4,5,6], [7,8,9]] 
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log("Input: [[1,2,3], [4,5,6], [7,8,9]] ");
+console.log("Expected value: 1+5+9 = 15, 3+5+7 = 15, 15+15 = 30");
 
-console.log(sumMatrixDiagonals(matrix));
+console.log("Result: ", sumMatrixDiagonals(matrix));
 
-// In terminal run node problem_1/matrixSum.js
+matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
+console.log("Input: [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]");
+console.log("Expected value: 1+6+11+16 = 34, 4+7+10+13 = 34, 34+34 = 68");
+
+console.log("Result: ", sumMatrixDiagonals(matrix));
+
+matrix = [
+    [1],
+    ];
+console.log("Input: [[1]]");
+console.log("Expected value: 1");
+
+console.log("Result: ", sumMatrixDiagonals(matrix));
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    ];  
+console.log("Input: [[1,2,3], [4,5,6]]");
+console.log("Expected value: Not a rectangle matrix");
+
+console.log("Result: ", sumMatrixDiagonals(matrix));
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
---
+++ problem_2-findOptimalChange.js
@@ -0,0 +1,36 @@
+const denom = [1, 2, 5, 10, 20, 50, 100, 500, 1000];
+
+function findOptimalChange(amount) {
+  // find edge cases
+  if (amount < 1) return "Amount must be greater than 0";
+  if (amount === 1) return 1;
+
+  let bills = 0;
+  let total = 0;
+  for (let i = denom.length - 1; i >= 0; i--) {
+    while (total + denom[i] <= amount) {
+      total = total + denom[i];
+      bills++;
+    }
+    if (total === amount) return bills;
+  }
+}
+
+console.log('Input: 70');
+console.log('Expected value: 50 + 20 = 2');
+console.log('Result: ', findOptimalChange(70));
+console.log('Input: 121');
+console.log('Expected value: 100 + 20 + 1 = 3');
+console.log('Result: ', findOptimalChange(121));
+console.log('Input: 17124');
+console.log('Expected value: (1000 x 17) + (100 x 1) + (20 x 1) + (2 x 2) = 21');
+console.log('Result: ', findOptimalChange(17124));
+console.log('Input: 1');
+console.log('Expected value: 1');
+console.log('Result: ', findOptimalChange(1));
+console.log('Input: 0');
+console.log('Expected value: Amount must be greater than 0');
+console.log('Result: ', findOptimalChange(0));
+console.log('Input: -1');
+console.log('Expected value: Amount must be greater than 0');
+console.log('Result: ', findOptimalChange(-1));
