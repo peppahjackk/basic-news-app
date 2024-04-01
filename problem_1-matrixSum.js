f3d4a2c0d2a5b6f03f645a3ea32fbe928d7d926f
Ted Moke <iam@tedmoke.com>
2024-04-01 08:28:13 -0400

    Cleanup

--- problem_1-matrixSum.js
+++ problem_1-matrixSum.js
@@ -4,7 +4,7 @@
 
 function sumMatrixDiagonals(matrix) {
   if (matrix.length === 0 || matrix[0].length < 1) return 0;
-  if (matrix.length !== matrix[0].length) return "Not a rectangle matrix";
+  if (matrix.length !== matrix[0].length) return "Not a square matrix";
   if (matrix.length === 1) return matrix[0][0];
 
   let sum = 0;
@@ -32,7 +32,7 @@ matrix = [
   [9, 10, 11, 12],
   [13, 14, 15, 16],
 ];
-console.log("Input: [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]");
+console.log("\nInput: [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]");
 console.log("Expected value: 1+6+11+16 = 34, 4+7+10+13 = 34, 34+34 = 68");
 
 console.log("Result: ", sumMatrixDiagonals(matrix));
@@ -40,7 +40,7 @@ console.log("Result: ", sumMatrixDiagonals(matrix));
 matrix = [
     [1],
     ];
-console.log("Input: [[1]]");
+console.log("\nInput: [[1]]");
 console.log("Expected value: 1");
 
 console.log("Result: ", sumMatrixDiagonals(matrix));
@@ -49,8 +49,8 @@ matrix = [
     [1, 2, 3],
     [4, 5, 6],
     ];  
-console.log("Input: [[1,2,3], [4,5,6]]");
-console.log("Expected value: Not a rectangle matrix");
+console.log("\nInput: [[1,2,3], [4,5,6]]");
+console.log("Expected value: Not a square matrix");
 
 console.log("Result: ", sumMatrixDiagonals(matrix));
 
